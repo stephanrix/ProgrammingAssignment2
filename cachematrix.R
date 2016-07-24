dff3552862d3783e6a1cda51218343e471a18fb9
stephanrix <stephan.rix@capgemini.com>
2016-07-24 22:08:27 +0200

    Update cachematrix
    Answer to programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This script inlcudes two functions to define cache communication functions and return an inverse matrix
 
-## Write a short comment describing this function
+# makeCacheMatrix creates a list of functions to retrieve a matrix from cache, write a matrix to cache, 
+# retrieve a matrix from cache, and write a matrix to cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the inverse of a matrix or retrieves it form cache if it exists. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
